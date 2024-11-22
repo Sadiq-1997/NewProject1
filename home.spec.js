ee4a606e855a514bc9b1b88ef5e4cca6d318d60b
Sadiq-1997 <basha11226@gmail.com>
2024-11-22 11:17:20 +0530

    file added

---
+++ home.spec.js
@@ -0,0 +1,13 @@
+const {chromium,test, expect,} = require("@playwright/test")
+
+test('browsing site', async () => {
+    const browse = await chromium.launch({
+        headless: false,
+    });  
+    const page = await browse.newPage();
+    await page.goto('https://darashaw.hrmthread.com/HrmThread/view/loginmain.aspx?1=1&key=darashaw&mod=ess&code=DCPL&usn=60021')
+    const pagetitle = await page.title();
+    await expect(page).toHaveTitle(pagetitle)
+    console.log(pagetitle);
+    
+})
